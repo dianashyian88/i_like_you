93864d9ec1d7304854cca1534345ac18d1319151
dianashyian88 <dvshyian@gmail.com>
2023-06-01 22:04:07 +0300

    create main

---
+++ main.py
@@ -0,0 +1,26 @@
+import time
+
+
+def main():
+    words = 'ike*You*I*l'
+    print('\nHi, sweetheart!\n\n')
+    for item in words.split():
+        letterlist = []
+        for y in range(12, -12, -1):
+            list_X = []
+            letters = ''
+            for x in range(-30, 30):
+                expression = ((x*0.05)**2+(y*0.1)**2-1)**3-(x*0.05)**2*(y*0.1)**3
+                if expression <= 0:
+                    letters += item[(x-y) % len(item)]
+                else:
+                    letters += ' '
+            list_X.append(letters)
+            letterlist += list_X
+        print('\n'.join(letterlist))
+        time.sleep(5)
+    print('Have a nice day! 💋\n')
+
+
+if __name__ == '__main__':
+    main()
